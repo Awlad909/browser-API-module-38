c15f965740e1c2ceb407bba5e7ed8a03b2ab9761
Awlad909 <you@example.com>
2021-09-06 10:50:23 +0600

    first added

---
+++ alert.js
@@ -0,0 +1,22 @@
+console.log('i am here');
+
+// alert('Ma is coming!!'');
+const maComing = () =>{
+    alert('Ammu is coming. Open the book');
+};
+
+const askPicnic = () =>{
+    const response = confirm('Are you going to picnic?');
+    console.log(response);
+    if(response === true){
+        alert('amake fee ta bkash kore de');
+    }
+    else{
+        console.log('DGM !!! Door e giya mor !!!!')
+    }
+};
+
+const askName = () =>{
+    const name = prompt('what is your name')
+    console.log(name);
+}
---
+++ explore.js
@@ -0,0 +1,9 @@
+const name = 15;
+const student = {name:'awlad', friend:'abir', job:'business',
+    makeMovie: function(name){
+        console.log(name);
+    }
+};
+student.makeMovie('din rat');
+// document.getElementById('name');
+
